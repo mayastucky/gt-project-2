f8eb6b8d337c8cf72d219ffde8477ba7c293dd81
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-17 12:50:20 -0500

    Seeds file started, just insert into statements, no values yet

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--GOES TO CHRISTINAS WORKBENCH FOR JAWSDB--> hard code for subject/specialities drop down in order to populate dropdown menu during register
+INSERT INTO specialties 
+
+
+INSERT INTO subjects 
