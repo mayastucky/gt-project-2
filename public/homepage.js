7cbb921ae74b9da98ea3d31d81f6245085faa140
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-18 14:20:32 -0500

    On click functions.  Do not work, but may be a jQuery requirement issue

--- public/homepage.js
+++ public/homepage.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
-//On click for login button
-//On click for Go! button
+    //On click for login button
+    const loginButton = $("#loginButton");
+    const getStartedButton = $("#getStartedButton");
+
+loginButton.on("click", function () {
+        console.log('Clicked login');
+        window.location.replace("/login");
+    });
+    //On click for Get Started button
+    getStartedButton.on("click", function () {
+        console.log('Clicked Get Started');
+        window.location.replace("/search");
+    });
+
 });
+
