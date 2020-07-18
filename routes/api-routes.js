0e29da9210b6d9c34eef3fb5f25a3ca5d552943b
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-18 09:46:02 -0500

    Post route for creating new educator via register path

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ const db = require("../models");
 module.exports = function(app) {
 
 //Get route to display member info
-//Not sure about the :id? do we need req.params
+//ID needs to come from the login click 
 //dashboard or login for path?
 app.get("/api/dashboard/:id", function(req, res) {
     db.Educator.findOne({
@@ -17,6 +17,19 @@ app.get("/api/dashboard/:id", function(req, res) {
   });
 
 //Create Route for new Member
+app.post("/api/register", function(req, res) {
+    console.log(req.body);
+    db.Educator.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password,
+      bio: req.body.bio,
+    })
+      .then(function(dbEducator) {
+        res.json(dbEducator);
+      });
+  });
 
 //Update Route for Updated Member
 
