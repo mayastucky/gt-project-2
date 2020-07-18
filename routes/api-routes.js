ce999bb68f4d13d3d7b9a1e1bef1288d2ee317fb
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-18 09:43:11 -0500

    Get route for displaying specific educator based on id

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,8 +3,23 @@ const db = require("../models");
 module.exports = function(app) {
 
 //Get route to display member info
+//Not sure about the :id? do we need req.params
+//dashboard or login for path?
+app.get("/api/dashboard/:id", function(req, res) {
+    db.Educator.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbEducator) {
+        res.json(dbEducator);
+      });
+  });
+
 //Create Route for new Member
+
 //Update Route for Updated Member
+
 //Delete Route for Delete Member
 
 };
