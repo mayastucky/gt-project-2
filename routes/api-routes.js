fab65d124cb4e8bb58bd221551371a924fee4f24
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-18 09:40:18 -0500

    Pseudocode api routes started

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,10 @@
 const db = require("../models");
 
 module.exports = function(app) {
+
+//Get route to display member info
+//Create Route for new Member
+//Update Route for Updated Member
+//Delete Route for Delete Member
+
 };
