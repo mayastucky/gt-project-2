de692d3b9078e6108f011516ac43b0397f7a0c43
kalebgarrison <kalebgarrison@gmail.com>
2020-07-18 20:42:20 -0400

    Added even listener to login.js

--- public/login.js
+++ public/login.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
-
+    const loginButton = $("#loginButton2");
+    // const emailInput = $("input#email-input");
+    // const passwordInput = $("input#password-input");
     //Click on login takes you to dashboard/:id based on hard coded thing? Redirect not ajax call I think according to Kevin
 
+    loginButton.on("click", function (event){
+        event.preventDefault();
+        console.log("You clicked me")
+        window.location.replace("/dashboard/1")
+    })
 });
